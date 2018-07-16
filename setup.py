ad7cbcb00ae428a3afa7239e8a82db7fc7cea455
Maximilian Menger <menger.maximilian@gmail.com>
2018-07-16 16:10:42 +0200

    added tests script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysharc',
+        version='0.1.1',
+        description='Python SHARC interface',
+        author='Maximilian F.S.J. Menger',
+        author_email='maximilian.menger@univie.ac.at',
+        scripts=['bin/sharc_test.py'],
+)
